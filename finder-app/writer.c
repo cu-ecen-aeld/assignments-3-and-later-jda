4c5289e453f88412782c2e33f267d635a3c76874
Jade Angrboða <jade@trashwitch.dev>
2023-05-27 21:19:31 +0800

    skel works

---
+++ finder-app/writer.c
@@ -0,0 +1,7 @@
+/* given 2 args, first is dir, second is search string,
+   count files in dir recursively and count number of times
+   search string appears in those files */
+
+int main(int argc, char **argv) {
+	return 0;
+}
