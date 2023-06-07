a9cb2d50452e9617b93745ac9b5faad4f87eacfa
Jade Angrboða <jade@trashwitch.dev>
2023-06-06 21:14:40 -0600

    works?!

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = f2b3bdb
+AESD_ASSIGNMENTS_VERSION = b5cd56f
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
